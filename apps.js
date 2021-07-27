1ca2279910306e8d9a99806c60424dd05fc793f7
Janet Azehko <Janet@projecterostribe.com>
2021-07-27 11:30:49 -0700

    Updated apps.js with code from class

--- apps.js
+++ apps.js
@@ -1,4 +1,4 @@
-function generateSales(averageSales, customerAtThatTime){
+ /*function generateSales(averageSales, customerAtThatTime){
     return averageSales * customerAtThatTime;
 }
 
@@ -25,4 +25,28 @@ const nextStore = {
     generateExplicit: function(){
         return generateSales(nextStore.averageSales, nextStore.customerAtThatTime);
     }
-};
+}; */
+
+
+// Notes from class 7:
+
+//alert("Loaded your JS");
+
+//we need to be able to keep yp with sales numbers byt he hours so weed need to have a list of the hours
+const businessHours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm"]
+//define an object literal
+let Seattle = {
+    // identify props
+    minCustomers: 23,
+    maxCustomer 65,
+    avgCookiesSale: 6.3,
+    customersPerHour: [],
+    totalDailyCookies:0,
+}
+
+//We need a generic random function customized to our needs.  So we can pass in the data we have
+//We need a random number of customers within the range of data we were given
+getCustomersPerHour: function(){
+    return (Math.floor(Math.random() * (maxCustomers - minCustomers +1)) + minCustomers);
+    //Use our random customer method
+}
